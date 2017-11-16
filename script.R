c5e71596a9e360eb53117e3ae022d140f716b5c9
andy92pac <andy.mpondo@gmail.com>
2017-11-16 16:14:22 +0100

    test

---
+++ script.R
@@ -0,0 +1,17 @@
+setwd('/Volumes/MacintoshHD/Users/Andy/Desktop/Kaggle')
+
+#Chargement des données
+svm_benchmark <- read.csv('data/svm_benchmark.csv')
+train <- read.csv('data/train.csv')
+
+#Séparation en 2 dataset training et validation
+library(caTools)
+
+set.seed(123)
+
+train$spl = sample.split(train[,1], SplitRatio = 0.7)
+training = train[train$spl==1,]
+validation = train[train$spl==0,]
+
+library(e1071)
+svm.model <- svm(training)
