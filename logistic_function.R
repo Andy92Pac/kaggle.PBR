565d9b50dee82237b00ae492c7aa92dc4d5b5177
Bylal Pro <farid.ahamada25@gmail.com>
2017-12-08 01:36:06 +0100

    glm feat backward

--- logistic_function.R
+++ logistic_function.R
@@ -1,10 +1,69 @@
+#Chargement des librairies----------------------------------------------------------
+library(caret)
+library(caTools)
+library(e1071)
+library(FactoMineR)
+library(randomForest)
+library(Rcmdr)
 
-#Dans un premier temps on fait une regression logistique binomiale
-#Binomiale car notre variable à expliquer est binaire 0 ou 1
-train_reg <- glm(y ~.,family=binomial(link='logit'),data=x)
-summary(train_reg)
+#Chargement des données-------------------------------------------------------------
+test <- read.csv('data/test.csv')
+train.data <- read.csv('data/train.csv')
 
-#J'expliquerai mieux demain jsuis fatigué morray mais le mode backward
-#Permet d'eliminer au fur est a mesure les variable non influente dans la prediction de notre variable à expliquer
-train_reg_back <- step(train_reg,direction="backward",trace=FALSE)
-summary(train_reg_back)
+#Présentation des données----------------------------------------------------------
+str(train.data)
+head(train.data)
+dim(train.data)
+summary(train.data)
+
+#Lavage des données-----------------------------------------------------------------
+
+remove.cols <- nearZeroVar(train.data[,-1], names = T)
+
+all.cols <- names(train.data)
+train.reduc <- train.data[,setdiff(all.cols, remove.cols)]
+
+train <- train.reduc
+
+#Division outcomename et predictornames----------------------------------------------------------
+
+outcomeName <- names(train)[1]
+predictorNames <- setdiff(names(train), outcomeName)
+
+#Splitage des données----------------------------------------------------------
+
+train$spl = sample.split(train[,1], SplitRatio = 0.7)
+training = train[train$spl==1,]
+validation = train[train$spl==0,]
+
+training <- subset(training, select = -c(spl))
+validation <- subset(validation, select = -c(spl))
+
+#Analyse factorielle----------------------------------------------------------
+
+PCA(training[,-1])
+
+#
+fitControl <- trainControl(
+  method = "repeatedcv",
+  number = 3,
+  repeats = 1,
+  savePredictions = 'final',
+  classProbs = T)
+
+training[,outcomeName]
+training[,outcomeName] <- as.factor(training[,outcomeName])
+#Construction du modèle de prediction : regression logistique ----------------------------------------------------
+
+#Methode glm en mode train 
+glm.model <- glm(training[,outcomeName] ~ ., data = training[,predictorNames],family = binomial(link = logit))
+summary(glm.model)
+res.glm <- stepwise(glm.model,direction="backward",criterion="AIC" )
+glm.model <- train(training[,predictorNames],training[,outcomeName],method='glm',trControl=fitControl,tuneLength=1, verbose = FALSE)
+glm.model
+
+
+#glm en mode train sur validation-------------------------------------------------------
+valid.predglm <- predict(glm.model,validation[,predictorNames],type = "prob")
+valid.predglm
+
