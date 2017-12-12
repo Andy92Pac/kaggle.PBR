a87ca20d23869fccb491fc1d513cfc3f2e71fb54
Bylalhood <farid.ahamada25@gmail.com>
2017-12-12 17:19:53 +0100

    Pour tester nos fonction de présentation des données et plot et cmpagnie

---
+++ datapresentation.R
@@ -0,0 +1,9 @@
+#Présentation des données
+test <- read.csv('data/test.csv')
+train.data <- read.csv('data/train.csv')
+
+str(train.data)
+head(train.data[,1:10],n = 6)
+h
+dim(train.data)
+str(train.data[,1:10])
